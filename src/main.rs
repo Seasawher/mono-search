37bc79c83bd170ac1f4f9ea74b0548235f162fd9
Seasawher <tellagoodstory@icloud.com>
2023-06-11 05:45:37 +0000

    ディレクトリを読み込んで，列挙する

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs;
+// use std::path;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -6,17 +8,20 @@ use clap::Parser;
 struct Args {
     /// Name of the person to greet
     #[arg(short, long)]
-    name: String,
+    target_dir_path: String,
 
     /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(short, long)]
+    file_name: String,
 }
 
 fn main() {
     let args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    let dir = fs::read_dir(&args.target_dir_path).unwrap();
+
+    for file in dir.into_iter() {
+        let file_name = file.unwrap().file_name();
+        println!("{:?}", file_name);
     }
 }
